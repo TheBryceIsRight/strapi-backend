c1032753072bd50c036d4e19e8a88d8c7a2edf1e
Bryce Watson <brycewatson315@gmail.com>
2022-12-28 13:53:10 -0500

    update for GCloud

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,19 +1,17 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
-
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: `/cloudsql/${env('INSTANCE_CONNECTION_NAME')}`, // for a MySQL database
+        // ⚠️ For a PostgreSQL database, use socketPath: `/cloudsql/${env('INSTANCE_CONNECTION_NAME')}` instead
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
       },
+      options: {},
     },
-    debug: false,
   },
 });
