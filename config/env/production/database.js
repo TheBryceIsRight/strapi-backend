0a7b4ab4f4431889d81580e6bc0d011831710a61
Bryce Watson <brycewatson315@gmail.com>
2023-01-02 15:01:12 -0500

    updated database code

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,17 +1,19 @@
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: `/cloudsql/${env('INSTANCE_CONNECTION_NAME')}`, // for a MySQL database
-        // ⚠️ For a PostgreSQL database, use socketPath: `/cloudsql/${env('INSTANCE_CONNECTION_NAME')}` instead
-        database: env('DATABASE_NAME'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
       },
-      options: {},
     },
+    debug: false,
   },
 });
--- config/server.js
+++ config/server.js
@@ -1,4 +1,9 @@
+// path: ./config/server.js
+
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
 });
