8863054af9a8c1547c9d83c00016d5ade23c2d05
Bryce Watson <brycewatson315@gmail.com>
2023-01-02 15:41:57 -0500

    preparing for deployment 2

--- config/server.js
+++ config/server.js
@@ -3,4 +3,5 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env("PUBLIC_URL", "https://strapi-backend-production-4fe2.up.railway.app/")
 });
