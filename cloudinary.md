c07d9555e7860fef78bd35bfe30f7034bd5c7363
Bryce Watson <brycewatson315@gmail.com>
2023-01-03 13:38:15 -0500

    cloudindary integration

---
+++ cloudinary.md
@@ -0,0 +1,5 @@
+cloudinary.config({ 
+  cloud_name: 'bryce-portfolio', 
+  api_key: '543657634933416', 
+  api_secret: 'uzYt94gN_ktrKH4IsVvU9eoLz2A' 
+});
--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,9 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '9331cfc8d812618e619158cede46e79b'),
+    secret: env('ADMIN_JWT_SECRET', 'e4350524b0689fc3f46e2407dae950e2'),
   },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
+  },
+
 });
