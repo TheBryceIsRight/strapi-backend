9e21cfabd505839c424a2cdcfa8f2027d95ab2d1
Bryce Watson <brycewatson315@gmail.com>
2023-01-03 09:43:18 -0500

    removed documentation plugin

--- config/server.js
+++ config/server.js
@@ -3,5 +3,4 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env("PUBLIC_URL", "https://strapi-backend-production-4fe2.up.railway.app/")
 });
